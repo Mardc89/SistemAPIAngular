0b834bd1a6c0022dcf7872e4db0c81124b9fb137
Mardc89 <mardc0602@gmail.com>
2025-07-31 13:57:53 -0500

    Agregando IDashBoardService

--- SistemAPIRest/Sistem.BLL/Implementacion/PedidoService.cs
+++ SistemAPIRest/Sistem.BLL/Implementacion/PedidoService.cs
@@ -1,10 +1,12 @@
 ﻿using MapsterMapper;
+using Microsoft.EntityFrameworkCore;
 using Sistem.BLL.Interfaces;
 using Sistem.DAL.Interfaces;
 using Sistem.DTO;
 using Sistem.Model;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -42,16 +44,66 @@ namespace Sistem.BLL.Implementacion
 
         }
 
-        public Task<List<PedidoDTO>> Historial(string buscarPor, string numeroPedido, string fechaInicio, string fechaFin)
+        public async Task<List<PedidoDTO>> Historial(string buscarPor, string numeroPedido, string fechaInicio, string fechaFin)
         {
-            throw new NotImplementedException();
+            IQueryable<Pedido> query = await _pedidoRepositorio.Consultar();
+            var ListaResultado=new List<Pedido>();
+
+            try
+            {
+                if (buscarPor=="fecha")
+                {
+                    DateTime fechInicio = DateTime.ParseExact(fechaInicio,"dd/MM/yyyy",new CultureInfo("es-PE"));
+                    DateTime fechFin = DateTime.ParseExact(fechaFin, "dd/MM/yyyy", new CultureInfo("es-PE"));
+
+                    ListaResultado = await query.Where(u =>u.FechaRegistro.Value.Date >= fechInicio.Date &&
+                     u.FechaRegistro.Value.Date <= fechFin.Date
+                     ).Include(m => m.DetallePedidos).ThenInclude(p => p.IdPedidoNavigation)
+                     .ToListAsync();
+                }
+                else
+                {
+                    ListaResultado = await query.Where(u => u.Numerodocumento==numeroPedido)
+                    .Include(m => m.DetallePedidos).ThenInclude(p => p.IdPedidoNavigation)
+                    .ToListAsync();
+
+                }
+
+                return _mapper.Map<List<PedidoDTO>>(ListaResultado);
+            }
+            catch
+            {
+
+                throw;
+            }
+
         }
 
 
 
-        public Task<List<ReporteDTO>> Reporte(string fechaInicio, string fechaFin)
+        public async Task<List<ReporteDTO>> Reporte(string fechaInicio, string fechaFin)
         {
-            throw new NotImplementedException();
+            IQueryable<DetallePedido> query = await _detallePedidoRepositorio.Consultar();
+            var ListaResultado = new List<DetallePedido>();
+
+            try
+            {
+                DateTime fechInicio = DateTime.ParseExact(fechaInicio, "dd/MM/yyyy", new CultureInfo("es-PE"));
+                DateTime fechFin = DateTime.ParseExact(fechaFin, "dd/MM/yyyy", new CultureInfo("es-PE"));
+                
+                ListaResultado = await query.Include(p => p.IdProductoNavigation)
+                    .Include(m=>m.IdPedidoNavigation)
+                    .Where(n=>n.IdPedidoNavigation.FechaRegistro.Value.Date>=fechInicio.Date &&
+                    n.IdPedidoNavigation.FechaRegistro.Value.Date >= fechFin.Date
+                    ).ToListAsync();
+
+                return _mapper.Map<List<ReporteDTO>>(ListaResultado);
+            }
+            catch
+            {
+
+                throw;
+            }
         }
     }
 }
